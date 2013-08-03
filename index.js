8602fb7856ddb4797b44b184fa22f556a0cfec91
Adrian Rossouw <adrian@daemon.co.za>
2013-08-03 04:25:45 +0200

    switch to server.js instead of index.js

--- index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./data/Mock.graft.js');
---
+++ server.js
@@ -0,0 +1,4 @@
+var Graft = require('graftjs/server');
+
+Graft.directory(__dirname);
+module.exports = require('./data/Mock.graft.js');
